080f0bc2e3d937dd2fcb1b7838cbc322eea922b3
ayeesha-anchi <ayeeshaanchi@appinessworld.com>
2022-05-19 08:35:11 +0530

    may18

--- firstApp/index.js
+++ firstApp/index.js
@@ -1,10 +1,88 @@
-const express = require("express");
-const app = express();
+// const express = require("express")
+// const app = express()
 
-app.use(() => {
-    console.log("we got a new request")
+// app.use((req,res) => {
+//     console.log("we got a new request")
+//     res.send("hello we got response here")
+// })
+
+
+
+//here we are using / to go to another page "routing"
+
+// app.get('/', (req ,res) => {
+//     res.send("this is our home page")
+// })
+
+// app.post('/cat', (req ,res) => {
+//     res.send("hey cat")
+// })
+
+// app.get('/cat', (req ,res) => {
+//     res.send("hey cat")
+// })
+
+// app.get('/dog', (req ,res) => {
+//     res.send("hey dog")
+// })
+
+// app.get('/r/:subreddit', (req, res) => {
+//     const { subreddit } = req.params;
+//     res.send(`<h1>browsing the ${subreddit} `)
+// })
+
+// app.get('/r/:subreddit/:postId', (req, res) => {
+//     const { subreddit ,postId } = req.params;
+//     res.send(`<h1>browsing the ${postId} from ${subreddit} `)
+// })
+
+// app.get('/search', (req, res) => {
+//     const { q } = req.query;
+//     if (!q) {
+//         res.send("nothing found")
+//     }
+//     res.send(`<h1>Hi from search ${q}</h1>`)
+// })
+
+// app.get('*', (req ,res) => {
+//     res.send("i don't know ")
+// })
+
+
+// app.listen(4000, () => {
+//     console.log("listening on port 4000")
+// })
+
+const express = require("express")
+const app = express()
+const path=require('path')
+
+app.set('view engine', 'ejs');
+app.set('firstApp', path.join(__dirname, 'views'))
+// app.get('/', (req, res)=>{
+// res.rendor('random.ejs')})
+
+app.get('/', (req ,res) => {
+    res.send("this is our page")
 })
 
-app.listen(3000, () => {
-    console.log("listening on port 3000")
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.send(`<h1>browsing the ${subreddit} `)
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random',{num})
+})
+
+app.get('/colors', (req, res) => {
+    const colors = [
+        'red','green','blue','yellow'
+    ]
+    res.render('colors',{colors})
+})
+
+app.listen(4000, () => {
+    console.log("listening on port 4000")
 })
